2e57a666d452dd5519741f4ccad6fcaab3a06ed8
dipesh subedi <dipeshfitness74@gmail.com>
2024-04-10 11:52:58 +0545

    first commit

--- dipesh.js
+++ dipesh.js
@@ -1,22 +1,7 @@
 var typed = new Typed("#typeWriter",{
-    strings:["","Web Desiginer","Web Developer","Graphic Desiginer","Youtuber","Freelancer","Logo Desiginer","Cyber Security","Social Media Manager","Student"],
-    typedSpeed:100,
-    BackSpeed:60,
+    strings:["Web Desiginer","Web Developer","","Youtuber","Freelancer","Logo Desiginer","Cyber Security"," Manager","Student"],
+    typedSpeed:20,
+    BackSpeed:20,
     loop:true
 })
 
-const nav = document.querySelector(".nav"),
-      navlist =nav.querySelector("li"),
-      totalNavList = navlist.length;
-      for(let i=0; i<totalNavList; i++)
-      {
-        
-        const a = navList[i].querySelector("a");  
-        a.addEventListener("click", function()
-        {
-            for(let j=0; j<totalNavList; j++){
-                navList[j].querySelector("a").classList.remove("active");
-            }
-            this.classList.add("active")
-        })
-      }
