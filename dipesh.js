ad433684ada0ae32a4fdb5b1ed8f358a0b74e85f
dipesh subedi <dipeshfitness74@gmail.com>
2024-04-10 15:21:02 +0545

    hello

--- dipesh.js
+++ dipesh.js
@@ -1,5 +1,5 @@
 var typed = new Typed("#typeWriter",{
-    strings:["Web Desiginer","Web Developer","","Youtuber","Freelancer","Logo Desiginer","Cyber Security"," Manager","Student"],
+    strings:[ "i'm web developer","Web Desiginer","Web Developer","","Youtuber","Freelancer","Logo Desiginer","Cyber Security"," Manager","Student"],
     typedSpeed:20,
     BackSpeed:20,
     loop:true
